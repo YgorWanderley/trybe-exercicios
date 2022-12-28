8b2be334b31fd3beb0e9bfb7798215722d3ac663
Ygor Wanderley <ygor.wanderley@hotmail.com>
2022-12-28 10:52:51 -0300

    finaliza corrida maluca

--- fundamentos/5.JS:DOM-eventos-WebStorage/dia-3-eventos/corrida-maluca/script.js
+++ fundamentos/5.JS:DOM-eventos-WebStorage/dia-3-eventos/corrida-maluca/script.js
@@ -16,11 +16,47 @@ window.onload = () => {
     const player1Win = parseInt(player1.style.marginLeft) > window.innerWidth;
     const player2Win = parseInt(player2.style.marginLeft) > window.innerWidth;
     if (player1Win) {
-      alert("PLAYER 1 GANHOU !!")  
+      alert("PLAYER 1 GANHOU !!")
+      resetCars();  
     }
     if (player2Win) {
       alert("PLAYER 2 GANHOU !!")
+      resetCars();
     }
   })
+  const resetCars = () => {
+    player1.style.marginLeft = 0;
+    player1.style.backgroundImage = "url('./files/selectPlayer.png')";
+    player2.style.marginLeft = 0;
+    player2.style.backgroundImage = "url('./files/selectPlayer.png')";
+  }
+
+  resetBtn.addEventListener("click", resetCars);
+
+  //Seleciona PLayer
+  const cars = document.getElementsByClassName("car");
+  const alternatives = document.getElementsByClassName("playersImages");
+
+  for (let index = 0; index <cars.length; index += 1) {
+    cars[index].addEventListener("click", event => {
+      console.log(cars[index]);
+      const selected = document.querySelector(".selected");
+      if (selected) {
+        selected.classList.remove("selected")
+      }
+      event.target.classList.add("selected");
+    })
+  }
+  for (index = 0; index < alternatives.length; index += 1) {
+    alternatives[index].addEventListener("click", event => {
+      const selected = document.querySelector(".selected");
+      if (selected) {
+        selected.style.backgroundImage = `url(${event.target.src})`;
+        selected.classList.remove("selected")
+      
+      }
+      
+    })
+  }
 
 }
