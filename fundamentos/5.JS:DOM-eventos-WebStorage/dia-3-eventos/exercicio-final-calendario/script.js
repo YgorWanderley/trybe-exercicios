83edb700c873afb2e8c026ab48e56a8ecd24cb17
Ygor Wanderley <ygor.wanderley@hotmail.com>
2023-01-06 15:05:47 -0300

    inicia passo 2

--- fundamentos/5.JS:DOM-eventos-WebStorage/dia-3-eventos/exercicio-final-calendario/script.js
+++ fundamentos/5.JS:DOM-eventos-WebStorage/dia-3-eventos/exercicio-final-calendario/script.js
@@ -16,6 +16,7 @@ const createDaysOfTheWeek = () => {
   // Escreva seu código abaixo.
   const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+  // Passo 1
   const createDaysOfTheMonth = () => {
     let getDaysList = document.querySelector("#days");
     for (let index = 0; index < decemberDaysList.length; index += 1) {
@@ -35,3 +36,8 @@ const createDaysOfTheWeek = () => {
     }
   }
   createDaysOfTheMonth();
+
+  // Passo 2
+  const createHolidayButton = (buttonName) => {
+    
+  }
